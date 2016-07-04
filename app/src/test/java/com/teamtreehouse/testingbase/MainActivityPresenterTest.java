96377a0b0924a2db67368987022b24deef5a67da
Venkat Gonuguntala <venkyg88@gmail.com>
2016-07-04 15:53:52 -0400

    initial unit test setup

---
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
@@ -0,0 +1,32 @@
+package com.teamtreehouse.testingbase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by venkatgonuguntala on 7/4/16.
+ */
+public class MainActivityPresenterTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void editTextUpdated() throws Exception {
+
+    }
+
+    @Test
+    public void colorSelected() throws Exception {
+
+    }
+
+    @Test
+    public void launchOtherActivityButtonClicked() throws Exception {
+
+    }
+
+}
