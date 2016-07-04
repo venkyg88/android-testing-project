2341e7dd1ebdd365146ed3a1b1c6bfbb2ff27328
Venkat Gonuguntala <venkyg88@gmail.com>
2016-07-04 15:55:10 -0400

    mocking using mockedView and testing `editTextUpdated()` method (arrange, act, assert)

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
@@ -9,14 +9,45 @@ import static org.junit.Assert.*;
  * Created by venkatgonuguntala on 7/4/16.
  */
 public class MainActivityPresenterTest {
+
+    MainActivityPresenter presenter;
+    MainActivityView view;
+
+
+    // Why?? It lets our app test without activity!!
+    public class MockedView implements MainActivityView {
+        String textViewText;
+        @Override
+        public void changeTextViewText(String text) {
+            textViewText = text;
+        }
+
+        @Override
+        public void changeBackGroundColor(int color) {
+
+        }
+
+        @Override
+        public void launchOtherActivity(Class activity) {
+
+        }
+    }
     @Before
     public void setUp() throws Exception {
-
+        view = new MockedView();
+        presenter = new MainActivityPresenter(view);
     }
 
     @Test
     public void editTextUpdated() throws Exception {
+        //arrange
+        String givenString = "test123";
 
+        //act
+        presenter.editTextUpdated(givenString);
+
+        //assert
+        assertEquals(givenString, ((MockedView)view).textViewText);
     }
 
     @Test
