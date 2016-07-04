6ae4497f1438e5a94c8704e95b1d495ec1afa3b1
Venkat Gonuguntala <venkyg88@gmail.com>
2016-07-04 18:29:24 -0400

    initial setup for roboelectric tests

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
@@ -1,12 +1,29 @@
 package com.teamtreehouse.testingbase;
 
+import android.os.Build;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 /**
  * Created by venkatgonuguntala on 7/4/16.
  */
 
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class/*, sdk = Build.VERSION_CODES.LOLLIPOP*/)
 public class MainActivityTest {
 
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void editTextUpdatesTextView() throws Exception{
+
+    }
 
 }
